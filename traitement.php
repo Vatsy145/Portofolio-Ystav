798cd3abfd34a532cc2072612971c0f25d7bcbd9
Va Tsy <80814790+Vatsy145@users.noreply.github.com>
2025-04-24 05:13:48 -0700

    func;added

--- traitement.php
+++ traitement.php
@@ -17,8 +17,8 @@ try {
     $mail->isSMTP();                                            //Send using SMTP
     $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'radoystav2218@gmail.com';                     //SMTP username
-    $mail->Password   = '';//validation par deux étapes                               //SMTP password
+    $mail->Username   = 'vatsynyavo6@gmail.com';                     //SMTP username
+    $mail->Password   = 'yofb wusm qpgu btqf';//validation par deux étapes                               //SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
     $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
